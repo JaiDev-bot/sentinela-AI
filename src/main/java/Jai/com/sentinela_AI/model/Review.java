3e4fb06221dd7c14a8ed570c7d3271eadbc96bde
Jaiane <jaianesoares700@gmail.com>
2025-12-17 15:17:50 -0300

    feat:(model) feito mapeamento de objeto relacional de 'Review'

---
+++ src/main/java/Jai/com/sentinela_AI/model/Review.java
@@ -0,0 +1,81 @@
+package Jai.com.sentinela_AI.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String texto;
+    private LocalDateTime dataHora;
+    private String sentimento;
+    private String keyImportant;
+    private String origem;
+
+    public Review(Long id, String texto, LocalDateTime dataHora, String sentimento, String keyImportant, String origem) {
+        this.id = id;
+        this.texto = texto;
+        this.dataHora = dataHora;
+        this.sentimento = sentimento;
+        this.keyImportant = keyImportant;
+        this.origem = origem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getSentimento() {
+        return sentimento;
+    }
+
+    public void setSentimento(String sentimento) {
+        this.sentimento = sentimento;
+    }
+
+    public String getKeyImportant() {
+        return keyImportant;
+    }
+
+    public void setKeyImportant(String keyImportant) {
+        this.keyImportant = keyImportant;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+}
+
