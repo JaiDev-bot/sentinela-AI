00b7f329c914e4dc0ed6508379d0d93e0032e68a
Jaiane <jaianesoares700@gmail.com>
2025-12-17 15:04:15 -0300

    first commit

---
+++ src/main/java/Jai/com/sentinela_AI/SentinelaAiApplication.java
@@ -0,0 +1,13 @@
+package Jai.com.sentinela_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jai/com/sentinela_AI/SentinelaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package Jai.com.sentinela_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
