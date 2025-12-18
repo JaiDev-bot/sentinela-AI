a12cae9189ceb793c631eefd965f953c7e047e1b
Jaiane <jaianesoares700@gmail.com>
2025-12-18 11:06:38 -0300

    feat: alterado método POST

--- src/main/java/Jai/com/sentinela_AI/controller/ReviewController.java
+++ src/main/java/Jai/com/sentinela_AI/controller/ReviewController.java
@@ -2,14 +2,14 @@ package Jai.com.sentinela_AI.controller;
 
 import Jai.com.sentinela_AI.model.Review;
 import Jai.com.sentinela_AI.service.ReviewService;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import java.util.Map;
+
 
 @RestController
 public class ReviewController {
@@ -21,15 +21,12 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @PostMapping
-    public ResponseEntity<Review> criarAnalise(@RequestBody Map<String, String> payload) {
-        String texto = payload.get("text");
-        String origem = payload.getOrDefault("source", "WEB");
-
-        Review resultado = reviewService.AnalisaESalva(texto, origem);
-
-        return new ResponseEntity<>(resultado, HttpStatus.CREATED);
+    @PostMapping("/analisar")
+    public ResponseEntity<Review> analisar(@RequestBody String texto) {
+        Review reviewSalva = reviewService.analisaESalva(texto, "Postman");
+        return ResponseEntity.ok(reviewSalva);
     }
+
 }
 
 
